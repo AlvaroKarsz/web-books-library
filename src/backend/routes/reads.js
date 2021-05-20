af2dd666679d216d991813b243baffb4153d7710
AlvaroKarsz <alvaorka@gmail.com>
2021-05-20 23:05:02 +0300

    Option to refetch descriptions for purchased wishes + read books

--- src/backend/routes/reads.js
+++ src/backend/routes/reads.js
@@ -36,7 +36,10 @@ module.exports = (app) => {
     res.send(await htmlRender.render({
       html: settings.SOURCE_CODE_HTML_DISPLAY_FILE_NAME,
       folder: settings.BOOKS_FOLDER_NAME,
-      displayer: entryDisplayer.build(readData, settings.BOOKS_FOLDER_NAME, {fetchRating: true})
+      displayer: entryDisplayer.build(readData, settings.BOOKS_FOLDER_NAME, {
+        fetchRating: true,
+        fetchDescription: true
+      })
     }));
   });
 
