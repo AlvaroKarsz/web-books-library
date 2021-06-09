15a485275394621dfb3446713ddb8f61d6e9a433
AlvaroKarsz <alvaorka@gmail.com>
2021-06-09 17:23:15 +0300

    fix timezone in logger module

--- src/backend/modules/logger.js
+++ src/backend/modules/logger.js
@@ -9,9 +9,23 @@ class Logger {
     this.NUMBER_OF_DASHED_LINES = 80;
   }
 
+  /*get date for relevant timezone*/
+  getTimeZonedDate() {
+    let date = new Date(),
+    tzOffset = date.getTimezoneOffset(), /*get timezone offset in minutes*/
+    epoch = +date, /*timestamp*/
+    timeZoneDate = new Date(epoch - tzOffset * 60000);  /*calculate timezoned date*/
+
+    return timeZoneDate;
+  }
+
+  getReadableDate() {
+    return new Date().toTimeString().split(' ')[0];
+  }
+
   getTodayLogPath() {
     /*generates today's log path based on today's date*/
-    const today = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Jerusalem"})),
+    const today = this.getTimeZonedDate(),
 
     year = today.getFullYear().toString(), /*taday's year*/
 
@@ -95,12 +109,11 @@ class Logger {
 
 
     */
-
     text = this.CONTENT_SEPARATOR.repeat(this.NUMBER_OF_DASHED_LINES) + /*top separator*/
     "\n" +
     type.toUpperCase() + /*type*/
     "\n" +
-     new Date().toLocaleString("en-US", {timeZone: "Asia/Jerusalem"}) + /*date*/
+    this.getReadableDate() + /*date*/
     "\n" +
     text + /*contents*/
     "\n" +
